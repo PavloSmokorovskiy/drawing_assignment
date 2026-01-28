3ffbc4adb8977a63c7f3a76be432a0f9b710e2a0
Pavlo <pavvelsm@gmail.com>
2026-01-28 23:26:16 +0100

    add CreateCanvasCommand

---
+++ src/main/java/drawing/CreateCanvasCommand.java
@@ -0,0 +1,8 @@
+package drawing;
+
+public record CreateCanvasCommand(int width, int height) implements Command {
+    @Override
+    public void execute(DrawingContext ctx) {
+        ctx.setCanvas(new Canvas(width, height));
+    }
+}
