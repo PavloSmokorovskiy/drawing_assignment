3f291cef30a693d21358ab87149a292579a61581
Pavlo <pavvelsm@gmail.com>
2026-01-29 00:12:57 +0100

    Refactor DrawRectangleCommand

--- src/main/java/drawing/DrawRectangleCommand.java
+++ src/main/java/drawing/DrawRectangleCommand.java
@@ -4,8 +4,16 @@ public record DrawRectangleCommand(Point corner1, Point corner2) implements Comm
 
     @Override
     public void execute(DrawingContext ctx) {
-        var canvas = ctx.requireCanvas();
-        canvas.validateBounds(corner1, corner2);
-        canvas.drawRectangle(corner1, corner2);
+        ctx.requireCanvas().validateBounds(corner1, corner2);
+
+        Point topLeft = new Point(Math.min(corner1.x(), corner2.x()), Math.min(corner1.y(), corner2.y()));
+        Point bottomRight = new Point(Math.max(corner1.x(), corner2.x()), Math.max(corner1.y(), corner2.y()));
+        Point topRight = new Point(bottomRight.x(), topLeft.y());
+        Point bottomLeft = new Point(topLeft.x(), bottomRight.y());
+
+        new DrawLineCommand(topLeft, topRight).execute(ctx); // top
+        new DrawLineCommand(bottomLeft, bottomRight).execute(ctx); // bottom
+        new DrawLineCommand(topLeft, bottomLeft).execute(ctx); // left
+        new DrawLineCommand(topRight, bottomRight).execute(ctx); // right
     }
 }
