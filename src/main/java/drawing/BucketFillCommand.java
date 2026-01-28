0193eea958ef1a188ade269c3e938d9a6b58712e
Pavlo <pavvelsm@gmail.com>
2026-01-28 23:35:33 +0100

    add BucketFillCommand

---
+++ src/main/java/drawing/BucketFillCommand.java
@@ -0,0 +1,10 @@
+package drawing;
+
+public record BucketFillCommand(Point point, char color) implements Command {
+    @Override
+    public void execute(DrawingContext ctx) {
+        var canvas = ctx.requireCanvas();
+        canvas.validateBounds(point);
+        canvas.fill(point, color);
+    }
+}
