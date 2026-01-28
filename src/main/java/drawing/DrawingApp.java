32d269911df231de8586546d0f1fcd5539cefe3e
Pavlo <pavvelsm@gmail.com>
2026-01-28 19:10:47 +0100

    Add input loop with an exit command

--- src/main/java/drawing/DrawingApp.java
+++ src/main/java/drawing/DrawingApp.java
@@ -1,7 +1,23 @@
 package drawing;
 
+import java.util.Scanner;
+
 public class DrawingApp {
     public static void main(String[] args) {
-        System.out.println("Drawing App");
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("\u001B[33mQ or q for exit");
+            System.out.print("\u001B[32mEnter command: ");
+            String input = scanner.nextLine();
+
+            if (input.equals("Q") || input.equals("q")) {
+                break;
+            }
+
+            System.out.println("\u001B[0mYou entered: " + input);
+        }
+
+        scanner.close();
     }
 }
