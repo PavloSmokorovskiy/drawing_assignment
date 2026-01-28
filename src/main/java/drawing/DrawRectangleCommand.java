82c9e0b774a54dba2004cebd115d78bb664f5b6f
Pavlo <pavvelsm@gmail.com>
2026-01-28 23:34:37 +0100

    add DrawRectangleCommand

---
+++ src/main/java/drawing/DrawRectangleCommand.java
@@ -0,0 +1,10 @@
+package drawing;
+
+public record DrawRectangleCommand(Point corner1, Point corner2) implements Command {
+    @Override
+    public void execute(DrawingContext ctx) {
+        var canvas = ctx.requireCanvas();
+        canvas.validateBounds(corner1, corner2);
+        canvas.drawRectangle(corner1, corner2);
+    }
+}
